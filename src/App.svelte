98a5a2cf3b251dcfc7d0973ad45eaea1b06cd607
Anton Klock <klock.anton@gmail.com>
2023-02-03 18:40:54 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<p>THIS IS LIVE UPDATING!</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #00fb54;
+		text-transform: capitalize;
+		font-size: 4em;
+		font-weight: 600;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
